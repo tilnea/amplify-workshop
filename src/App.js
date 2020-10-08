a6a40c8ce8d0df3187912bb21fcd1ec33fa5efd7
Sanne Wintrén <sannewintren@Sannes-MacBook-Pro.local>
2020-10-08 19:31:13 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { API, Auth, Storage } from "aws-amplify";
+import { AmplifySignOut, withAuthenticator } from "@aws-amplify/ui-react";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 
-function App() {
+import Button from "./Button";
+import CreatePost from "./CreatePost";
+import Header from "./Header";
+import Post from "./Post";
+import Posts from "./Posts";
+import { css } from "emotion";
+import { listPosts } from "./graphql/queries";
+
+function Router() {
+  /* create a couple of pieces of initial state */
+  const [showOverlay, updateOverlayVisibility] = useState(false);
+  const [posts, updatePosts] = useState([]);
+
+  /* fetch posts when component loads */
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+  async function fetchPosts() {
+    /* query the API, ask for 100 items */
+    let postData = await API.graphql({
+      query: listPosts,
+      variables: { limit: 100 },
+    });
+    let postsArray = postData.data.listPosts.items;
+    /* map over the image keys in the posts array, get signed image URLs for each image */
+    postsArray = await Promise.all(
+      postsArray.map(async (post) => {
+        const imageKey = await Storage.get(post.image);
+        post.image = imageKey;
+        return post;
+      })
+    );
+    /* update the posts array in the local state */
+    setPostState(postsArray);
+  }
+  async function setPostState(postsArray) {
+    updatePosts(postsArray);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <HashRouter>
+        <div className={contentStyle}>
+          <Header />
+          <hr className={dividerStyle} />
+          <Button
+            title="New Post"
+            onClick={() => updateOverlayVisibility(true)}
+          />
+          <Switch>
+            <Route exact path="/">
+              <Posts posts={posts} />
+            </Route>
+            <Route path="/post/:id">
+              <Post />
+            </Route>
+          </Switch>
+        </div>
+        <AmplifySignOut />
+      </HashRouter>
+      {showOverlay && (
+        <CreatePost
+          updateOverlayVisibility={updateOverlayVisibility}
+          updatePosts={setPostState}
+          posts={posts}
+        />
+      )}
+    </>
   );
 }
 
-export default App;
+const dividerStyle = css`
+  margin-top: 15px;
+`;
+
+const contentStyle = css`
+  min-height: calc(100vh - 45px);
+  padding: 0px 40px;
+`;
+
+export default withAuthenticator(Router);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "./index.css";
+
+import * as serviceWorker from "./serviceWorker";
+
+import Amplify from "aws-amplify";
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom";
+import config from "./aws-exports";
+Amplify.configure(config);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
